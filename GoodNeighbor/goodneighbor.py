9b78c4869f32db209f63fdff09d612ffa4b1814a
Cam Parra <caeparra@gmail.com>
2016-09-21 08:51:33 -0600

    the goodneighbor has arrived

--- GoodNeighbor/goodneighbor.py
+++ GoodNeighbor/goodneighbor.py
@@ -1,16 +1,18 @@
 import numpy as np
 from sklearn import datasets as ds
 
+
 iris = ds.load_iris()
+bc = ds.load_breast_cancer
 
 
-class HardCodedClassifier:
+class GoodNeighborClassifier:
     def __init__(self):
         pass
 
     @staticmethod
     def train_machine(train_set, train_target):
-        print("Machine is trained, me Lord.")
+
 
     @staticmethod
     def predict(train_set):
@@ -25,15 +27,7 @@ class HardCodedClassifier:
     @staticmethod
     def k_nearest(k, data, data_class, inputs):
 
-        size_of_data = int(input("How many dimensions does your array have?\n>> "))
-
         n_inputs = inputs.shape[0]
-        if (size_of_data % 2 == 0 and k % 2 == 0) or ():
-            print("Both K and the dimensions are even making k odd\n...")
-            k += 1
-            print("K is now ", k)
-
-
         closest = np.zeros(n_inputs)
 
         for x in range(n_inputs):
@@ -53,8 +47,3 @@ class HardCodedClassifier:
 
 
 
-hc = HardCodedClassifier()
-randomarray = np.array([[1.8, 2.8, 4.1, 2.1], [5.8, 2.8, 5.1, 2.4], [5.8, 2.8, 5.1, 2.4]])
-hc.k_nearest(4, iris.data, iris.target, randomarray)
-
-print(hc.k_nearest(4, iris.data, iris.target, randomarray))
