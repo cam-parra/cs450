f940692d9339052233018f0efc51400601d35cd6
Cam Parra <caeparra@gmail.com>
2016-09-21 08:11:53 -0600

    the goodneighbor has arrived

--- HardCodedClassifier/goodneighbor.py
+++ HardCodedClassifier/goodneighbor.py
@@ -22,21 +22,29 @@ class HardCodedClassifier:
 
         return x
 
+    @staticmethod
+    def k_nearest(k, data, data_class, inputs):
 
-    def k_nearest(self, k, data, data_class, inputs):
+        size_of_data = int(input("How many dimensions does your array have?\n>> "))
 
         n_inputs = inputs.shape[0]
+        if (size_of_data % 2 == 0 and k % 2 == 0) or ():
+            print("Both K and the dimensions are even making k odd\n...")
+            k += 1
+            print("K is now ", k)
+
+
         closest = np.zeros(n_inputs)
 
         for x in range(n_inputs):
 
-            distances = np.sum((data - inputs[x, :])**2, axis=1)
+            distances = np.sum((data - inputs[x, :]) ** 2, axis=1)
             indicies = np.argsort(distances, axis=0)
             classes = np.unique(data_class[indicies[:k]])
             if len(classes) == 1:
                 closest[x] = np.unique(classes)
             else:
-                counts = np.zeros(max(classes)+1)
+                counts = np.zeros(max(classes) + 1)
                 for i in range(k):
                     counts[data_class[indicies[i]]] += 1
                 closest[x] = np.max(counts)
@@ -44,8 +52,9 @@ class HardCodedClassifier:
             return closest
 
 
+
 hc = HardCodedClassifier()
-randomarray = np.array([[ 5.8, 2.8 , 5.1,  2.4]])
+randomarray = np.array([[1.8, 2.8, 4.1, 2.1], [5.8, 2.8, 5.1, 2.4], [5.8, 2.8, 5.1, 2.4]])
 hc.k_nearest(4, iris.data, iris.target, randomarray)
 
 print(hc.k_nearest(4, iris.data, iris.target, randomarray))
