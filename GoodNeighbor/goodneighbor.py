a91fbe151114ebaff8051ad98cf7b4d6c64767c2
Cam Parra <caeparra@gmail.com>
2016-09-24 20:57:30 -0600

    hello

--- GoodNeighbor/goodneighbor.py
+++ GoodNeighbor/goodneighbor.py
@@ -1,49 +1,49 @@
 import numpy as np
+import pandas as pd
 from sklearn import datasets as ds
-
+from sklearn.cross_validation import train_test_split as tsp
+from sklearn import preprocessing as pre
 
 iris = ds.load_iris()
-bc = ds.load_breast_cancer
+bc = ds.load_breast_cancer()
+# read in data and add a header colum
+car = pd.read_csv('./cars.csv', names=['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'target'])
+# make all of the row data except the last item
+data_car = car.ix[:, :-1]
+target_cat = car.target
 
 
 class GoodNeighborClassifier:
-    def __init__(self):
-        pass
+    target = None
+    data = None
+    k = None
+    mean = None
+    std = None
+    cl = None
 
-    @staticmethod
-    def train_machine(train_set, train_target):
+    def set_cl(self, cla):
+        self.cl = cla
 
+    def k_nearest(self, inputs):
 
-    @staticmethod
-    def predict(train_set):
-        x = []
-        for i in train_set:
-            x.append("setsoa")
-        percent = (len(x) / 150) * 100
-        print(len(train_set), "where tested. ", percent, "% are setosa.")
+        dist = np.sum((self.data - inputs) ** 2, axis=1)
+        idx = np.argsort(dist, axis=0)
+        classes = np.unique(self.target[idx[:self.k]])
+        if len(classes) == 1:
+            closest = np.unique(classes)[0]
+        else:
+            counts = np.zeros(max(classes) + 1)
+            for i in range(self.k):
+                counts[self.target[idx[i]]] += 1
+            closest = np.max(counts)
 
-        return x
+        return closest
+    def train(self, training, train_t):
+        self.k =  3 if len(self.cl) != 3 else 5
+        self.target = train_t
+        x = np.asarray(training)
+        self.mean = x.mean()
+        self.std = x.std()
+        self.data = pre.normalize(training, norm='l2')
 
-    @staticmethod
-    def k_nearest(k, data, data_class, inputs):
-
-        n_inputs = inputs.shape[0]
-        closest = np.zeros(n_inputs)
-
-        for x in range(n_inputs):
-
-            distances = np.sum((data - inputs[x, :]) ** 2, axis=1)
-            indicies = np.argsort(distances, axis=0)
-            classes = np.unique(data_class[indicies[:k]])
-            if len(classes) == 1:
-                closest[x] = np.unique(classes)
-            else:
-                counts = np.zeros(max(classes) + 1)
-                for i in range(k):
-                    counts[data_class[indicies[i]]] += 1
-                closest[x] = np.max(counts)
-
-            return closest
-
-
-
+good = GoodNeighborClassifier()
